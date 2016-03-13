dd9e88c4a776d05e857d5268a8325f790479bfa2
AddiEgg <ade3@hi.is>
2016-03-13 16:30:47 +0000

    hello wrold 4

--- app/src/main/java/school/throstur/backgammonandroid/TrophyActivity.java
+++ app/src/main/java/school/throstur/backgammonandroid/TrophyActivity.java
@@ -21,7 +21,7 @@ public class TrophyActivity extends AppCompatActivity {
         int addi = 1;
         int egg = 2;
         int fff = 3;
-        int oneTime = 466;
+        int oneTime = 4666;
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
