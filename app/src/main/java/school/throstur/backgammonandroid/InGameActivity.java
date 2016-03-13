59471ec10808f3ea56ffeaa0aca853bad8a81fc5
thth80 <thth80@hi.is>
2016-03-13 16:49:08 +0000

    Test fyrir version control pælingar

--- app/src/main/java/school/throstur/backgammonandroid/InGameActivity.java
+++ app/src/main/java/school/throstur/backgammonandroid/InGameActivity.java
@@ -23,7 +23,7 @@ public class InGameActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Replace with your hello Addi action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
