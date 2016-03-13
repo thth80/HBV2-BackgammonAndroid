fe379d5f5026303150a91e3a35035f07d34b3097
thth80 <thth80@hi.is>
2016-03-13 16:50:36 +0000

    Test 2 fyrir version control pælingar - ignore it

--- app/src/main/java/school/throstur/backgammonandroid/TrophyActivity.java
+++ app/src/main/java/school/throstur/backgammonandroid/TrophyActivity.java
@@ -23,7 +23,7 @@ public class TrophyActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "TEST 2", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
