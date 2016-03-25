2bb10c938bcba01c077156126d9e6bf78c4ea8a2
thth80 <thth80@hi.is>
2016-03-25 17:17:18 +0000

    Bætti við Fragments folder og bjó til tóm ListsFragment og SetupMatchFragment

---
+++ app/src/main/java/school/throstur/backgammonandroid/Fragments/ListsFragment.java
@@ -0,0 +1,9 @@
+package school.throstur.backgammonandroid.Fragments;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by Þröstur on 25.3.2016.
+ */
+public class ListsFragment extends Fragment {
+}
